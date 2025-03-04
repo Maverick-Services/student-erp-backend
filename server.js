9065c815d34a7eaf2244b4baeee02a4200194a6b
Maverick-Services <maverick@example.com>
2025-03-04 14:57:23 +0530

    cors --update

--- server.js
+++ server.js
@@ -21,13 +21,7 @@ app.use(cors());
 
 const allowedOrigins = ["http://localhost:3000", "https://yourfrontenddomain.com"];
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: "*",
     credentials: true,
 }));
 // Middleware
